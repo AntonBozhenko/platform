03c8b32fe8a24ae09782bb3caf4faaf2e6d68ec8
AntonBozhenko <Kekshpeker@yandex.ru>
2023-05-24 14:16:17 +0300

    switch to ProTable and add choose of column show

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import React, {type FC} from 'react';
+import React, {useState, type FC} from 'react';
 import styles from './App.module.css';
-import {Table, Typography} from 'antd';
+import {Typography} from 'antd';
 import {colums, key} from './initial-data/report-config';
 import {data} from './initial-data/data';
+import ProTable from '@ant-design/pro-table';
 const {Title} = Typography;
 
 export const App: FC = () => (
 	<>
-		<Typography>
-			<Title level={3} className={styles.title}>{data.name} {data.code}</Title>
-		</Typography>
-		<Table columns={colums} dataSource={data.items} rowKey={key} />
+		<Title level={3} className={styles.title}>{data.name} {data.code}</Title>
+		<ProTable
+			columns={colums}
+			dataSource={data.items}
+			rowKey={key}
+			search={false}
+		/>
 	</>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'antd/dist/reset.css';
 import {App} from './App';
+import {ConfigProvider} from 'antd';
+import ruRu from 'antd/locale/ru_RU';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root')!,
 );
 root.render(
-	<App />,
+	<ConfigProvider locale={ruRu}>
+		<App />
+	</ConfigProvider>,
 );
